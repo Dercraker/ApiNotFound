93678a899a2ea9db1551f59e1579cfac6775bfc0
Dercraker <antoine.capitain@gmail.com>
2022-10-05 16:29:25 +0200

    user ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,17 +2,44 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Error;
 use App\Entity\Messages;
 use App\Entity\Pictures;
 use App\Repository\MessagesRepository;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+  private $userPasswordHasher;
+  public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+  {
+    $this->faker = Factory::create('fr_FR');
+    $this->userPasswordHasher = $userPasswordHasher;
+  }
+
+
+
   public function load(ObjectManager $manager): void
   {
+    //* Authentified Users
+    for ($i = 0; $i < 10; $i++) {
+      $userUser = new User();
+      $password = $this->faker->password(2, 6);
+      $userUser->setUsername($this->faker->userName() . '@' . $password)
+        ->setRoles(["ROLE_USER"])
+        ->setPassword($this->userPasswordHasher->hashPassword($userUser, $password));
+      $manager->persist($userUser);
+    }
+
+    $adminUser = new User();
+    $adminUser->setUsername("ADMIN")
+      ->setRoles(["ROLE_ADMIN"])
+      ->setPassword($this->userPasswordHasher->hashPassword($adminUser, "password"));
+    $manager->persist($adminUser);
 
     //Get Json data from file and decode it
     $json = file_get_contents(__DIR__ . '/codes.json');
